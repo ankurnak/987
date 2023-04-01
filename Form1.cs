383fa3db878486e4a0b99c08c0aa489dd91b3a3d
ankurnak <112162607+ankurnak@users.noreply.github.com>
2023-04-01 15:10:09 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp25
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double x = double.Parse(textBox1.Text);
+                double y = double.Parse(textBox2.Text);
+
+
+                double radius = Math.Sqrt(x * x + y * y);
+                double angle = Math.Atan2(y, x);
+
+
+                textBox3.Text = radius.ToString();
+                textBox4.Text = angle.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter valid numbers for the real and imaginary parts.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                textBox5.Clear();
+                textBox6.Clear();
+            }
+        }
+    }
+}
